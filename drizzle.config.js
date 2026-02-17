20b3a6097b8657db453ee4d9b84133db0fc9a5b7
Lochini <luhradik1@gmail.com>
2026-02-17 23:26:16 +0530

    set up the database

---
+++ drizzle.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from "drizzle-kit";
+
+export default defineConfig({
+  dialect: "postgresql",
+  schema: "./utils/schema.jsx",
+  out: "./drizzle",
+  dbCredentials: {
+    url: process.env.NEXT_PUBLIC_DATABASE_URL,
+  },
+});
