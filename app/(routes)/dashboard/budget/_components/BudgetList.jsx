3d42d129a5bbe71facc8fce5d00c94c94f7a0bd9
Lochini <luhradik1@gmail.com>
2026-02-18 15:16:37 +0530

    display budget list on budgets page

--- app/(routes)/dashboard/budget/_components/BudgetList.jsx
+++ app/(routes)/dashboard/budget/_components/BudgetList.jsx
@@ -1,11 +1,45 @@
-import React from 'react'
+"use client"
+
+import { useEffect, useState } from 'react'
 import CreateBudget from './CreateBudget'
+import { db } from '@/utils/dbConfigs'
+import { getTableColumns, sql, eq } from 'drizzle-orm'
+import { Budgets, Expenses } from '@/utils/schema'
+import { useUser } from '@clerk/nextjs'
+import BudgetItem from './BudgetItem'
 
 function BudgetList() {
+
+    const { user } = useUser();
+    const [budgetList, setBudgetList] = useState([]);
+
+    const getBudgetList = async () => {
+        if (!user?.primaryEmailAddress?.emailAddress) return;
+
+        const result = await db.select({
+            ...getTableColumns(Budgets),
+            totalSpend: sql`sum(${Expenses.amount})`.mapWith(Number),
+            totalItem: sql`count(${Expenses.id})`.mapWith(Number)
+        })
+            .from(Budgets)
+            .leftJoin(Expenses, eq(Budgets.id, Expenses.budgetID))
+            .where(eq(Budgets.createdBy, user.primaryEmailAddress.emailAddress))
+            .groupBy(Budgets.id);
+
+        setBudgetList(result);
+    }
+
+    useEffect(() => {
+        user && getBudgetList();
+    }, [user]);
+
     return (
         <div className='mt-7'>
-            <div className='grid grid-col-1 md:grid-cols-2 lg:grid-cols-3'>
+            <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
                 <CreateBudget />
+                {budgetList?.map((budget) => (
+                    <BudgetItem key={budget.id} budget={budget} />
+                ))}
             </div>
         </div>
     )
